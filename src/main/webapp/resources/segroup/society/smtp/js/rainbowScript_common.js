b9543b7d01ee5ed8307bc085c0cd7c9d113bd918
honghyunAhn <anh2073@naver.com>
2021-11-12 22:26:49 +0900

    update

--- src/main/webapp/resources/segroup/society/smtp/js/rainbowScript_common.js
+++ src/main/webapp/resources/segroup/society/smtp/js/rainbowScript_common.js
@@ -9,15 +9,16 @@ function togglePlay() {
 }
 
 // [ onclick event ] --> modal show/hide
-function openModal() {
-  const modal = document.querySelector('.modal');
-  function first(){
+function openModal(url) {
+  let modal = document.querySelector('.modal.' + url);
+  
+  function first() {
     modal.style.display = "block";
     document.body.classList.add("not-scroll");
   }
-  function second(){
-    modal.addEventListener("click", function(){
-      if(document.body.classList.contains("not-scroll")){
+  function second() {
+    document.addEventListener("click", function (e) {
+      if (e.target === modal) {
         closeModal();
       }
     })
@@ -25,31 +26,32 @@ function openModal() {
   first();
   second();
 }
-
 function closeModal() {
-	alert();
-  document.querySelector('.modal').style.display = "none";
+  let modal = document.querySelectorAll('.modal');
+  for (var i = 0; i < modal.length; i++) {
+    modal[i].style.display = "none";
+  }
   document.body.classList.remove("not-scroll");
 }
 
-window.addEventListener("load", function() {
-  
+window.addEventListener("load", function () {
+
   // 나의강의실 tab
   const tabList = document.querySelectorAll('.tab_headerWrap .tab_header a');
   const contents = document.querySelectorAll('.tab_bodyWrap .tab_body')
   let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
 
   for (var i = 0; i < tabList.length; i++) {
-      tabList[i].addEventListener('click', function (e) {
-          e.preventDefault();
-          for (var j = 0; j < tabList.length; j++) {
-              tabList[j].parentNode.classList.remove('active');
-              contents[j].style.display = 'none';
-          }
-          this.parentNode.classList.add('active');
-          activeCont = this.getAttribute('tab');
-          document.querySelector(activeCont).style.display = 'block';
-      });
-  }  
+    tabList[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      for (var j = 0; j < tabList.length; j++) {
+        tabList[j].parentNode.classList.remove('active');
+        contents[j].style.display = 'none';
+      }
+      this.parentNode.classList.add('active');
+      activeCont = this.getAttribute('tab');
+      document.querySelector(activeCont).style.display = 'block';
+    });
+  }
 
 });
